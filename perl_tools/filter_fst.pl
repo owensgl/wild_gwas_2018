338ae94b136e71cbf91ae6dbb6cccc8ce1ea886b
Gregory Owens <gregory.owens@alumni.ubc.ca>
2018-12-18 10:23:41 -0800

    New more filtered ID snps

--- perl_tools/filter_fst.pl
+++ perl_tools/filter_fst.pl
@@ -2,11 +2,11 @@
 use warnings;
 use strict;
 
-#This takes the fst.txt.gz (including extra two columns for XRQ, and outputs the sites above a threshold for Fst and 0/1 heterozygosity)
+#This takes the fst.txt.gz (including extra two columns for XRQ, and outputs the sites above a threshold for Freq and 0/1 heterozygosity)
 
 my $min_het = 0.5;
-my $min_fst = 0.8;
-
+#my $min_fst = 0.8;
+my $min_freq = 0.8;
 while(<STDIN>){
   chomp;
   if ($. == 1){
@@ -14,8 +14,9 @@ while(<STDIN>){
   }else{
     my @a = split(/\t/,$_);
     my $fst = $a[8];
+    my $freq = $a[9];
     my $het = $a[12];
-    if ($fst >= $min_fst){
+    if ($freq >= $min_freq){
       if ($min_het >= $min_het){
         print "\n$_";
       }
