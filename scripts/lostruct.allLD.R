3519ea10d6353a5ad6db085cefb965886a806bc4
Gregory Owens <gregory.owens@alumni.ubc.ca>
2018-12-17 16:34:23 -0800

    Fixed location

--- scripts/lostruct.allLD.R
+++ scripts/lostruct.allLD.R
@@ -4,16 +4,11 @@ library(ggthemes)
 pdf("Annuus.tranche90.snp.env.90.bi.remappedHa412.maf10.thin5k.500kbwin.ld.pdf",height=10,width=10)
 for (i in sprintf("%02d", seq(17))){
   
-  ld <- read_tsv(paste("/media/owens/Copper/wild_gwas_2018/annuus/Annuus.tranche90.snp.env.90.bi.remappedHa412.maf10.thin5k.Chr",i,".geno.ld",sep=""),
-                 col_names = c("chr","pos1","pos2","n","r2"),skip=1)
+  ld <- read_tsv(paste("/media/owens/Copper/wild_gwas_2018/argophyllus/Argophyllus.tranche90.snp.gwas.90.bi.remappedHa412HO.thin1k.maf5.Chr",i,".windows.ld.gz",sep=""))
   window_size <- 500000
-  print(
   ld %>%
-    mutate(win1 = floor(pos1/window_size)*window_size, win2 = floor(pos2/window_size)*window_size) %>% 
-    group_by(win1, win2) %>%
-    arrange(desc(r2)) %>%
-    slice(2) %>% 
-    ggplot(.,aes(x=win1/1000000,y=win2/1000000)) + geom_tile(aes(fill=r2)) +
+    mutate(percent_above_20 = r2_min_0.2/n) %>%
+    ggplot(.,aes(x=win1/1000000,y=win2/1000000)) + geom_tile(aes(fill=max_2_r2)) +
     theme_bw() + scale_fill_viridis_c(name="2nd highest R2") +
     xlab("MB") + ylab("MB") +
     ggtitle(paste("HA412TMPChr",i,sep=""))
