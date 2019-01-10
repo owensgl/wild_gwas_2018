4e8c931d13357e509cd0250d94b1732d64e5c287
Gregory Owens <gowens@cedar1.cedar.computecanada.ca>
2019-01-09 17:01:59 -0800

    Fixed for Ha412 remap

--- perl_tools/genotype_inv_from_freq_printsites.pl
+++ perl_tools/genotype_inv_from_freq_printsites.pl
@@ -6,23 +6,28 @@ use warnings;
 my $mds_fst_file = $ARGV[0]; #List of fst outliers
 my $gzvcf = $ARGV[1];
 #my $min_freq = 0.95;
-open MDS, $mds_fst_file;
+my $Ha412_remap = "TRUE"; #Set to TRUE to output the remapped positions
+
+open (MDS, "gunzip -c $mds_fst_file |");
 
 my %allele_1;
 my %allele_2;
+my %ha412_chr;
+my %ha412_pos;
 while(<MDS>){
   chomp;
   if ($. == 1){next;}
   my @a = split(/\t/,$_);
   my $chr = $a[0];
   my $pos = $a[1];
-  my $freq = $a[7];
-  my $mds = $a[10];
-  my $inv_0_allele = $a[8];
-  my $inv_2_allele = $a[9];
-#  if ($freq <= $min_freq){next;}
-  $allele_1{$chr}{$pos}{$mds} = $inv_0_allele;
-  $allele_2{$chr}{$pos}{$mds} = $inv_2_allele;
+  my $ha412_chr = $a[2];
+  my $ha412_pos = $a[3];
+  $ha412_chr{$chr.$pos}= $ha412_chr;
+  $ha412_pos{$chr.$pos}= $ha412_pos;
+  my $inv_0_allele = $a[10];
+  my $inv_2_allele = $a[11];
+  $allele_1{$chr}{$pos} = $inv_0_allele;
+  $allele_2{$chr}{$pos} = $inv_2_allele;
 }
 close MDS;
 open(IN, "gunzip -c $gzvcf |");
@@ -30,7 +35,7 @@ open(IN, "gunzip -c $gzvcf |");
 my %sample;
 my %counts;
 my $site_count = 0;
-print "chr\tpos\tmds_coord\tsample\tgenotype\tdepth";
+print "XRQchr\tXRQpos\tchr\tpos\tsample\tgenotype\tdepth";
 while(<IN>){
   chomp;
   if ($_ =~ m/^##/){
@@ -52,21 +57,19 @@ while(<IN>){
   my $ref = $a[3];
   my $alt = $a[4];
   my @alts = split(/,/,$alt);
-#  print "Found outlier at $chr $pos\n";
-  foreach my $mds (sort keys %{$allele_1{$chr}{$pos}}){
     #Check to make sure that the alleles match;
     #Find out which allele in this site matches with the inversion alleles
     my $allele_1 = "NA";
     my $allele_2 = "NA";
-    if ($ref eq $allele_1{$chr}{$pos}{$mds}){
+    if ($ref eq $allele_1{$chr}{$pos}){
       $allele_1 = 0;
-    }elsif ($ref eq $allele_2{$chr}{$pos}{$mds}){
+    }elsif ($ref eq $allele_2{$chr}{$pos}){
       $allele_2 = 0;
     }
     foreach my $x (0..$#alts){
-       if ($alts[$x] eq $allele_1{$chr}{$pos}{$mds}){
+       if ($alts[$x] eq $allele_1{$chr}{$pos}){
          $allele_1 = ($x+1);
-       }elsif ($alts[$x] eq $allele_2{$chr}{$pos}{$mds}){
+       }elsif ($alts[$x] eq $allele_2{$chr}{$pos}){
          $allele_2 = ($x+1);
        }
      }
@@ -84,25 +87,17 @@ while(<IN>){
     foreach my $i (9..$#a){
       my @fields = split(/:/,$a[$i]);
       my $genotype = $fields[0];
-<<<<<<< HEAD:genotype_inv_from_freq_printsites.pl
-<<<<<<< HEAD:genotype_inv_from_freq_printsites.pl
       my $dp = $fields[$dp_n];
       if ($dp == 0){next;}
-=======
-      my $dp = $fields[2];
->>>>>>> 144e4c7faf11e51653b5de32255e2c2f212aa55d:genotype_inv_from_freq_printsites.pl
-=======
-      my $dp = $fields[2];
->>>>>>> 144e4c7faf11e51653b5de32255e2c2f212aa55d:genotype_inv_from_freq_printsites.pl
       if (($genotype eq '.') or ($genotype eq './.')){next;}
       if (($genotype eq "$allele_1\/$allele_2") or  ($genotype eq "$allele_2\/$allele_1")){
-	$counts{$sample{$i}}{$mds}{1}++;
-        print "\n$chr\t$pos\t$mds\t$sample{$i}\t01\t$dp";
+	$counts{$sample{$i}}{1}++;
+        print "\n$chr\t$pos\t$ha412_chr{$chr.$pos}\t$ha412_pos{$chr.$pos}\t$sample{$i}\t01\t$dp";
       }elsif ($genotype eq "$allele_1\/$allele_1"){
-	print "\n$chr\t$pos\t$mds\t$sample{$i}\t00\t$dp";      
+	print "\n$chr\t$pos\t$ha412_chr{$chr.$pos}\t$ha412_pos{$chr.$pos}\t$sample{$i}\t00\t$dp";      
       }elsif ($genotype eq "$allele_2\/$allele_2"){
-	print "\n$chr\t$pos\t$mds\t$sample{$i}\t11\t$dp";
+	print "\n$chr\t$pos\t$ha412_chr{$chr.$pos}\t$ha412_pos{$chr.$pos}\t$sample{$i}\t11\t$dp";
       }
-    }
+    
   }
 }  
