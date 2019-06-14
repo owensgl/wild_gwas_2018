9207fde7d96d115246ec1b6197bc476944e03210
Gregory Owens <gregory.owens@alumni.ubc.ca>
2019-06-14 10:33:41 -0700

    Null W script

--- scripts/null_W_env.R
+++ scripts/null_W_env.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(qvalue)
 
 env_associations <- read_tsv("/media/owens/Copper/wild_gwas/env_associations/for_Greg/var_out_annuus_2018_HA412_maf_0.03_all_covariates_BayPass_IS_STD.txt",
                              col_names = c("chr_pos","chr","pos", "Latitude_e","Longitude_e","Elevation_e","MAT_e",
@@ -75,11 +76,14 @@ env_associations %>%
 env_associations %>%
   sample_n(.,10000) -> null_10k_set
 
+variable_n <- 3:28 #The columns with environmental variables
 
+inv_z_scores <- tibble(chr=character(),mds=character(),variable=character(),z=numeric())
 #Get Z scores for inversions
 for (i in 1:nrow(ann_inversion_list)){
   chosen_chr <- ann_inversion_list$chr[i]
   chosen_mds <- ann_inversion_list$mds[i]
+  print(paste("Testing",chosen_chr,chosen_mds))
   env_associations_inv <- env_associations %>% filter(chr == "NANA") #Create empty tibble
   for( n in 1:nrow(ann_inversions)){
     if (chosen_chr != ann_inversions$chr[n]){next}
@@ -88,43 +92,121 @@ for (i in 1:nrow(ann_inversion_list)){
       filter(chr == chosen_chr, pos > ann_inversions$start[n], pos < ann_inversions$end[n]) -> tmp
     env_associations_inv <- rbind(env_associations_inv, tmp)
   }
-  inv_scores <- env_associations_inv[,3] %>% pull()
-  
-  length_inv_scores <- length(inv_scores)
-  
-  null_10k_scores  <- null_10k_set[,3] %>% pull()
-  length_null_10k_scores <- 10000
-  w_test <- wilcox.test(inv_scores,null_10k_scores)
-  
-  Z <- (2*w_test$statistic - (length_inv_scores*length_null_10k_scores))/
-    sqrt(length_inv_scores * length_null_10k_scores*(length_inv_scores + length_null_10k_scores + 1) / 3)
-  w_test
-  
-  
+  for (n in variable_n){
+    inv_scores <- env_associations_inv[,n] %>% pull()
+    
+    length_inv_scores <- length(inv_scores)
+    
+    null_10k_scores  <- null_10k_set[,n] %>% pull()
+    length_null_10k_scores <- 10000
+    w_test <- wilcox.test(inv_scores,null_10k_scores)
+    
+    Z <- (2*w_test$statistic - (length_inv_scores*length_null_10k_scores))/
+      sqrt(length_inv_scores * length_null_10k_scores*(length_inv_scores + length_null_10k_scores + 1) / 3)
+    tmp_z_scores <- tibble(chr=chosen_chr,mds=chosen_mds,variable=colnames(env_associations_inv)[n],z=Z)
+    inv_z_scores <- rbind(inv_z_scores,tmp_z_scores)
+  }
+
 }
 
+perm_z_scores <- tibble(chr=character(),mds=character(),variable=character(),z=numeric())
 
-#Get scores for nor null_window_sets
+#Get scores for nor null_window_sets. Looped for each inversion to make window number match (i.e. more windows for larger inversions)
 n_windows <- 29
 perm_repeats <- 1000
-perm_vec <- rep("", times=perm_repeats)
-for (i in 1:1000){
-  perm_windows <- windows_outside_inversions %>%
-    sample_n(.,n_windows) 
-  null_window_scores <- env_associations_noninv %>%
-    semi_join(.,perm_windows) %>% .[,3] %>% pull() 
-  length_null_window_scores <- length(null_window_scores)
-  
-  null_10k_scores  <- null_10k_set[,3] %>% pull()
-  length_null_10k_scores <- 10000
-  w_test <- wilcox.test(null_window_scores,null_10k_scores)
-  
-  Z <- (2*w_test$statistic - (length_null_window_scores*length_null_10k_scores))/
-    sqrt(length_null_window_scores * length_null_10k_scores*(length_null_window_scores + length_null_10k_scores + 1) / 3)
-  w_test
-  perm_vec[i] <- Z
+
+for (x in 1:nrow(ann_inversion_list)){
+  chosen_chr <- ann_inversion_list$chr[x]
+  chosen_mds <- ann_inversion_list$mds[x]
+  chosen_windows <- ann_inversions_length %>% filter(chr == chosen_chr, mds == chosen_mds) %>% pull(length)
+  print(paste("Permuting",chosen_chr,chosen_mds))
+  for (i in 1:perm_repeats){
+    if (i %% 100 == 1){
+      print(paste("Permutation",i))
+    }
+    perm_windows <- windows_outside_inversions %>%
+      sample_n(.,chosen_windows) 
+    null_windows <- env_associations_noninv %>%
+      semi_join(.,perm_windows) 
+    length_null_window_scores <- nrow(null_windows)
+    
+    for (n in variable_n){
+      
+      null_window_scores <- null_windows %>% .[,n] %>% pull()
+      null_10k_scores  <- null_10k_set[,n] %>% pull()
+      length_null_10k_scores <- 10000
+      
+      w_test <- wilcox.test(null_window_scores,null_10k_scores)
+      
+      Z <- (2*w_test$statistic - (length_null_window_scores*length_null_10k_scores))/
+        sqrt(length_null_window_scores * length_null_10k_scores*(length_null_window_scores + length_null_10k_scores + 1) / 3)
+      tmp_z_scores <- tibble(chr=chosen_chr,mds=chosen_mds,variable=colnames(env_associations_inv)[n],z=Z)
+      perm_z_scores <- rbind(perm_z_scores, tmp_z_scores)
+    }
+  }
 }
 
 
-  hist(as.numeric(perm_vec))
+
+#Compare permutation Z scores to inversion Z scores
+
+inv_p_scores <- tibble(chr=character(),mds=character(),variable=character(),empirical_p=numeric())
+for (i in 1:nrow(ann_inversion_list)){
+  chosen_chr <- ann_inversion_list$chr[i]
+  chosen_mds <- ann_inversion_list$mds[i]
+  print(paste("Testing",chosen_chr,chosen_mds))
+  for (n in variable_n){
+    chosen_variable <- colnames(env_associations_inv)[n]
+    inv_score <- inv_z_scores %>%
+      filter(chr == chosen_chr, mds == chosen_mds, variable == chosen_variable) %>% pull(z)
+    perm_z_scores %>% 
+      filter(chr == chosen_chr, mds == chosen_mds, variable == chosen_variable) %>% 
+      mutate(rank = case_when(z < inv_score ~"Lower",
+                       z >= inv_score ~ "Higher")) %>%
+      group_by(rank,.drop=FALSE) %>%
+      summarize(count=n()) -> ranking
+    if (nrow(ranking) == 1){
+      empirical_p <- 1/(ranking$count[1]+1)
+    }else{
+      empirical_p <- (ranking$count[1]+1)/(ranking$count[2] +ranking$count[1] + 1)
+    }
+    tmp_p_scores <- tibble(chr=chosen_chr,mds=chosen_mds,variable=colnames(env_associations_inv)[n],empirical_p=empirical_p)
+    inv_p_scores <- rbind(inv_p_scores,tmp_p_scores)
+  }
+}
+    
+inv_p_scores    
+#Make qvalues for each trait
+tmp_p_scores <- inv_p_scores %>% filter(chr == "NANANA") 
+for (n in variable_n){
+  chosen_variable <- colnames(env_associations_inv)[n]
+  inv_p_scores %>% filter(variable == chosen_variable) -> tmp
+  tmp$q <- qvalue(tmp$empirical_p, pi0 = 1)$qvalues
+  tmp_p_scores <- rbind(tmp_p_scores, tmp)
+}
+inv_p_scores <- tmp_p_scores   
+
+pdf("var_out_annuus_2018_HA412_maf_0.03_all_covariates_BayPass_IS_STD.null_w.pdf")
+inv_p_scores %>%
+  mutate(qsig = case_when(q < 0.05 ~ "sig",
+                          TRUE ~ "nonsig"),
+         pmax = case_when(empirical_p <0.000999002 ~ "max",
+                          TRUE ~ "nonmax")) %>%
+  mutate(chr_mds = paste0(chr,"_",mds)) %>%
+  ggplot(.) + geom_tile(aes(y=variable,x=chr_mds,fill=qsig)) +
+  scale_fill_manual(values=c("white","grey")) +
+  geom_point(data=inv_p_scores %>%
+               mutate(qsig = case_when(q < 0.05 ~ "sig",
+                                       TRUE ~ "nonsig"),
+                      pmax = case_when(empirical_p <0.000999002 ~ "max",
+                                       TRUE ~ "nonmax")) %>%
+               mutate(chr_mds = paste0(chr,"_",mds)) %>% 
+               filter(pmax == "max"), aes(y=variable, x=chr_mds))  +
+  theme_bw() + 
+  theme(axis.text.x = element_text(angle = 60, hjust = 1))
+dev.off()
+
+
+
+
 
